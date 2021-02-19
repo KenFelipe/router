58fdca3d1d8063acd60b5aed64d6865dfdc2fb62
Felipe Kenzo <Kenzo12152@gmail.com>
2021-02-18 23:03:56 -0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: './bundle.js',
+        path: path.join(__dirname, '/public')
+    },
+    devServer: {
+        contentBase: './public',
+        port: 8080,
+        open: false
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js[x]?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        '@babel/preset-env',
+                        '@babel/preset-react'
+                    ]
+                }
+            },
+        ]
+    }
+}
